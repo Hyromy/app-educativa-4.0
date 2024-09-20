fbc828075c1e7d5fc575d8991f16eb58a1520ff8
Hyromy <formulajoel9@gmail.com>
2024-09-20 11:33:29 -0600

    prueba de fragmentos de vista

--- app/app/src/main/java/com/example/app/ui/prueba_fragmento/PruebaFragmentoViewModel.java
+++ app/app/src/main/java/com/example/app/ui/prueba_fragmento/PruebaFragmentoViewModel.java
@@ -1,16 +1,15 @@
-package com.example.app.ui.home;
+package com.example.app.ui.prueba_fragmento;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
-
+public class PruebaFragmentoViewModel extends ViewModel {
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public PruebaFragmentoViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Este es un fragmento, se puede editar desde ui>nombreFragmento>ViewModel.java");
+        mText.setValue("Este es mi fragmento de prueaba");
     }
 
     public LiveData<String> getText() {
