c0ee21f0ef3981c26067ae0e85cc64f78d695526
Hyromy <formulajoel9@gmail.com>
2024-11-08 09:56:22 -0600

    crud resultado examen y expansion de tabla

--- app/app/src/main/java/com/example/app/db/models/ResultadoExamenModel.java
+++ app/app/src/main/java/com/example/app/db/models/ResultadoExamenModel.java
@@ -9,12 +9,14 @@ public class ResultadoExamenModel implements Serializable {
     public static final String id = "id_resultado_examen";
     public static final String idUsuario = "id_usuario";
     public static final String idExamen = "id_examen_diagostico";
+    public static final String puntajeObtenido = "puntaje_obtenido";
     public static final String nivelObtenido = "nivel_obtenido";
 
     // ---- valores ----
     public int idValue;
     public int idUsuarioValue;
     public int idExamenValue;
+    public int puntajeObtenidoValue;
     public int nivelObtenidoValue;
 
     // ---- sentencias ----
@@ -23,6 +25,7 @@ public class ResultadoExamenModel implements Serializable {
             id + " integer primary key autoincrement, " +
             idUsuario + " integer not null, " +
             idExamen + " integer not null, " +
+            puntajeObtenido + " integer, " +
             nivelObtenido + " integer, " +
             "foreign key (" + idUsuario + ") references " + UsuarioModel.tbName + " (" + UsuarioModel.id + ") on delete cascade on update cascade, " +
             "foreign key (" + idExamen + ") references " + ExamenDiagnosticoModel.tbName + " (" + ExamenDiagnosticoModel.id + ") on delete cascade on update cascade)";
@@ -34,6 +37,7 @@ public class ResultadoExamenModel implements Serializable {
         this.idValue = 0;
         this.idUsuarioValue = 0;
         this.idExamenValue = 0;
+        this.puntajeObtenidoValue = 0;
         this.nivelObtenidoValue = 0;
     }
 
@@ -41,11 +45,13 @@ public class ResultadoExamenModel implements Serializable {
             int id,
             int idUsuario,
             int idExamen,
+            int puntajeObtenido,
             int nivelObtenido
     ) {
         this.idValue = id;
         this.idUsuarioValue = idUsuario;
         this.idExamenValue = idExamen;
+        this.puntajeObtenidoValue = puntajeObtenido;
         this.nivelObtenidoValue = nivelObtenido;
     }
 
@@ -55,6 +61,7 @@ public class ResultadoExamenModel implements Serializable {
                 id + "=" + idValue + ", " +
                 idUsuario + "=" + idUsuarioValue + ", " +
                 idExamen + "=" + idExamenValue + ", " +
+                puntajeObtenido + "=" + puntajeObtenidoValue + ", " +
                 nivelObtenido + "=" + nivelObtenidoValue + "}";
     }
 }
---
+++ app/app/src/main/java/com/example/app/db/utils/crud/ResultadoExamen.java
@@ -0,0 +1,149 @@
+package com.example.app.db.utils.crud;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.app.db.models.ResultadoExamenModel;
+
+public class ResultadoExamen extends AbstractCRUD<ResultadoExamenModel> {
+    public ResultadoExamen(Context context) {
+        super(context);
+    }
+
+    @Override
+    public int getIdBy(String arg, String value) {
+        SQLiteDatabase db = getReadableDatabase();
+        int id = 0;
+
+        Cursor cursor = db.query(
+            ResultadoExamenModel.tbName,
+            colums(),
+            arg + " = ?",
+            new String[] {value},
+            null,
+            null,
+            null
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            id = cursor.getInt(0);
+            cursor.close();
+        } else if (cursor != null) {
+            cursor.close();
+        }
+
+        return id;
+    }
+
+    @Override
+    public long insert(ResultadoExamenModel obj) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(ResultadoExamenModel.idUsuario, obj.idUsuarioValue);
+        values.put(ResultadoExamenModel.idExamen, obj.idExamenValue);
+        values.put(ResultadoExamenModel.puntajeObtenido, obj.puntajeObtenidoValue);
+        values.put(ResultadoExamenModel.nivelObtenido, obj.nivelObtenidoValue);
+
+        return db.insert(ResultadoExamenModel.tbName, null, values);
+    }
+
+    @Override
+    public ResultadoExamenModel read(int id) {
+        SQLiteDatabase db = getReadableDatabase();
+        ResultadoExamenModel resultadoExamen = null;
+
+        Cursor cursor = db.query(
+            ResultadoExamenModel.tbName,
+            colums(),
+            ResultadoExamenModel.id + " = ?",
+            new String[] {String.valueOf(id)},
+            null,
+            null,
+            null
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            resultadoExamen = new ResultadoExamenModel(
+                cursor.getInt(0),
+                cursor.getInt(1),
+                cursor.getInt(2),
+                cursor.getInt(3),
+                cursor.getInt(4)
+            );
+            cursor.close();
+        } else if (cursor != null) {
+            cursor.close();
+        }
+
+        return resultadoExamen;
+    }
+
+    @Override
+    public ResultadoExamenModel[] readAll() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(
+            ResultadoExamenModel.tbName,
+            colums(),
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+        int size = cursor.getCount();
+        ResultadoExamenModel[] resultadoExamenes = new ResultadoExamenModel[size];
+        for (int i = 0; i < size; i++) {
+            cursor.moveToPosition(i);
+            resultadoExamenes[i] = new ResultadoExamenModel(
+                cursor.getInt(0),
+                cursor.getInt(1),
+                cursor.getInt(2),
+                cursor.getInt(3),
+                cursor.getInt(4)
+            );
+        }
+        cursor.close();
+
+        return resultadoExamenes;
+    }
+
+    @Override
+    public int update(ResultadoExamenModel obj) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(ResultadoExamenModel.idUsuario, obj.idUsuarioValue);
+        values.put(ResultadoExamenModel.idExamen, obj.idExamenValue);
+        values.put(ResultadoExamenModel.puntajeObtenido, obj.puntajeObtenidoValue);
+        values.put(ResultadoExamenModel.nivelObtenido, obj.nivelObtenidoValue);
+
+        return db.update(
+            ResultadoExamenModel.tbName,
+            values,
+            ResultadoExamenModel.id + " = ?",
+            new String[] {String.valueOf(obj.idValue)}
+        );
+    }
+
+    @Override
+    public int delete(ResultadoExamenModel obj) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        return db.delete(
+            ResultadoExamenModel.tbName,
+            ResultadoExamenModel.id + " = ?",
+            new String[] {String.valueOf(obj.idValue)}
+        );
+    }
+
+    public int nextId() {
+        return super.nextId(ResultadoExamenModel.tbName, ResultadoExamenModel.id);
+    }
+
+    public String[] colums() {
+        return super.colums(ResultadoExamenModel.tbName);
+    }
+}
